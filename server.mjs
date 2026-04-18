8e3b5414381e502e270eb5ded508f5c84f682f22
tehnplk <tehnplk@gmail.com>
2026-04-18 10:42:09 +0700

    fix(server): default to port 3001

--- server.mjs
+++ server.mjs
@@ -4,7 +4,7 @@ import { Server } from "socket.io";
 
 const dev = process.env.NODE_ENV !== "production";
 const hostname = process.env.DB_HOST || "localhost";
-const port = parseInt(process.env.PORT || "3000", 10);
+const port = parseInt(process.env.PORT || "3001", 10);
 const app = next({ dev, hostname, port });
 const handler = app.getRequestHandler();
 
